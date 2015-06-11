f8ac3aaeb9176da03dec1cd8a2d5af6932f96bff
Kurt <kaphotics@gmail.com>
2015-06-10 23:26:33 -0700

    Refactoring
    Compressed declarations and assignments, commented out unused lines,
    removed unnecessary operations.

--- Fire Emblem Awakening Save Tool/Huffman8.cs
+++ Fire Emblem Awakening Save Tool/Huffman8.cs
@@ -2,9 +2,7 @@
  * Mostly taken from huffman.c: http://www.romhacking.net/utilities/826/
  */
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Fire_Emblem_Awakening_Save_Tool
 {
@@ -22,10 +20,10 @@ namespace Fire_Emblem_Awakening_Save_Tool
         const uint HUF_RCHAR = 0x40;
         const uint HUF_NEXT = 0x3F;
 
-        const uint RAW_MINIM = 0x0;
-        const uint RAW_MAXIM = 0xFFFFFF;
+        // const uint RAW_MINIM = 0x0;
+        // const uint RAW_MAXIM = 0xFFFFFF;
 
-        const uint HUF_MINIM = 0x4;
+        // const uint HUF_MINIM = 0x4;
         const uint HUF_MAXIM = 0x1400000;
 
         class HuffmanNode
@@ -38,7 +36,6 @@ namespace Fire_Emblem_Awakening_Save_Tool
             public HuffmanNode lson;
             public HuffmanNode rson;
         }
-
         class HuffmanCode
         {
             public uint nbits;
@@ -48,47 +45,39 @@ namespace Fire_Emblem_Awakening_Save_Tool
         HuffmanNode[] tree;
         byte[] codetree, codemask;
         HuffmanCode[] codes;
-        uint max_symbols = 0x100, num_leafs, num_nodes;
+        const uint max_symbols = 0x100;
+        uint num_leafs, num_nodes;
         uint[] freqs;
         int num_bits;
 
         public byte[] Decompress(byte[] data)
         {
-            byte* pak;
-            byte* raw;
-            byte* pak_end;
-            byte* raw_end;
-            uint pak_len, raw_len, header;
-            byte* tree;
-            uint pos, next, mask4, code, ch;
-            int nbits;
-
-            pak_len = (uint)data.Length;
+            uint pak_len = (uint)data.Length;
 
             fixed (byte* pak_buffer = &data[0])
             {
-                header = *pak_buffer;
+                uint header = *pak_buffer;
                 num_bits = (int)(header & 0xF);
-                raw_len = *(uint*)pak_buffer >> 8;
+                uint raw_len = *(uint*)pak_buffer >> 8;
                 byte[] raw_data = new byte[raw_len];
                 fixed (byte* raw_buffer = &raw_data[0])
                 {
-                    pak = pak_buffer + 4;
-                    raw = raw_buffer;
-                    pak_end = pak_buffer + pak_len;
-                    raw_end = raw_buffer + raw_len;
+                    byte* pak = pak_buffer + 4;
+                    byte* raw = raw_buffer;
+                    byte* pak_end = pak_buffer + pak_len;
+                    byte* raw_end = raw_buffer + raw_len;
 
-                    tree = pak;
+                    byte* tree = pak;
                     pak += (*pak + 1) << 1;
 
-                    nbits = 0;
+                    int nbits = 0;
 
-                    pos = *(tree + 1);
-                    next = 0;
+                    uint pos = *(tree + 1);
+                    uint next = 0;
 
-                    mask4 = 0;
+                    uint mask4 = 0;
 
-                    code = *(uint*)pak;
+                    uint code = *(uint*)pak;
 
                     while (raw < raw_end)
                     {
@@ -102,6 +91,7 @@ namespace Fire_Emblem_Awakening_Save_Tool
 
                         next += ((pos & HUF_NEXT) + 1) << 1;
 
+                        uint ch;
                         if ((code & mask4) == 0)
                         {
                             ch = pos & HUF_LCHAR;
@@ -125,40 +115,29 @@ namespace Fire_Emblem_Awakening_Save_Tool
                         }
                     }
 
-                    raw_len = (uint)(raw - raw_buffer);
+                    // raw_len = (uint)(raw - raw_buffer);
                     return raw_data;
                 }
             }
         }
-
         public byte[] Compress(byte[] data)
         {
-            byte* pak;
-            byte* raw;
-            byte* pak_end;
-            byte* raw_end;
-            byte* cod;
-            uint raw_len, pak_len, len, new_len;
-            HuffmanCode code;
-            int cwork;
-            byte mask;
+            // byte* pak_end;
+            // uint new_len;
             uint* pk4 = stackalloc uint[1];
-            uint mask4, ch;
-            int nbits;
 
             num_bits = 8;
-            raw_len = (uint)data.Length;
+            uint raw_len = (uint)data.Length;
 
             fixed (byte* raw_buffer = &data[0])
             {
                 byte[] pbuf = new byte[HUF_MAXIM + 1];
                 fixed (byte* pak_buffer = &pbuf[0])
                 {
-                    pak_len = 4 + (max_symbols << 1) + raw_len + 3;
                     *(uint*)pak_buffer = (CMD_CODE) | (raw_len << 8);
-                    pak = pak_buffer + 4;
-                    raw = raw_buffer;
-                    raw_end = raw_buffer + raw_len;
+                    byte* pak = pak_buffer + 4;
+                    byte* raw = raw_buffer;
+                    byte* raw_end = raw_buffer + raw_len;
                     HUF_InitFreqs();
                     HUF_CreateFreqs(raw_buffer, (int)raw_len);
 
@@ -173,24 +152,25 @@ namespace Fire_Emblem_Awakening_Save_Tool
 
                     fixed (byte* cd = &codetree[0])
                     {
-                        cod = cd;
-                        len = (uint)((*cod + 1) << 1);
+                        byte* cod = cd;
+                        uint len = (uint)((*cod + 1) << 1);
                         while (len-- != 0) *pak++ = *cod++;
-                        mask4 = 0;
+                        uint mask4 = 0;
                         while (raw < raw_end)
                         {
-                            ch = *raw++;
+                            uint ch = *raw++;
 
 
+                            int nbits;
                             for (nbits = 8; nbits != 0; nbits -= num_bits)
                             {
-                                code = codes[ch & ((1 << num_bits) - 1)];
+                                HuffmanCode code = codes[ch & ((1 << num_bits) - 1)];
                                 ////  code = codes[ch >> (8 - num_bits)];
 
                                 len = code.nbits;
-                                cwork = 0;
+                                int cwork = 0;
 
-                                mask = (byte)HUF_MASK;
+                                byte mask = (byte)HUF_MASK;
                                 while (len-- != 0)
                                 {
                                     if ((mask4 >>= HUF_SHIFT) == 0)
@@ -211,7 +191,7 @@ namespace Fire_Emblem_Awakening_Save_Tool
                                 ////  ch = (ch << num_bits) & 0xFF;
                             }
                         }
-                        pak_len = (uint)(pak - pak_buffer);
+                        uint pak_len = (uint)(pak - pak_buffer);
                         return pbuf.Take((int)pak_len).ToArray();
                     }
                 }
@@ -226,16 +206,14 @@ namespace Fire_Emblem_Awakening_Save_Tool
 
             for (i = 0; i < max_symbols; i++) freqs[i] = 0;
         }
-
         private void HUF_CreateFreqs(byte* raw_buffer, int raw_len)
         {
-            uint ch;
-            int nbits;
             uint i;
 
             for (i = 0; i < raw_len; i++)
             {
-                ch = *raw_buffer++;
+                uint ch = *raw_buffer++;
+                int nbits;
                 for (nbits = 8; nbits != 0; nbits -= num_bits)
                 {
                     freqs[ch >> (8 - num_bits)]++;
@@ -272,22 +250,17 @@ namespace Fire_Emblem_Awakening_Save_Tool
             }
             num_nodes = (num_leafs << 1) - 1;
         }
-
         private void HUF_InitTree()
         {
             tree = new HuffmanNode[num_nodes];
             uint i;
             for (i = 0; i < num_nodes; i++) tree[i] = null;
         }
-
         private void HUF_CreateTree()
         {
-            HuffmanNode lnode;
-            HuffmanNode rnode;
-            uint lweight, rweight, num_node;
             uint i;
 
-            num_node = 0;
+            uint num_node = 0;
             for (i = 0; i < max_symbols; i++)
             {
                 if (freqs[i] != 0)
@@ -307,8 +280,10 @@ namespace Fire_Emblem_Awakening_Save_Tool
 
             while (num_node < num_nodes)
             {
-                lnode = rnode = null;
-                lweight = rweight = 0;
+                HuffmanNode rnode;
+                HuffmanNode lnode = rnode = null;
+                uint rweight;
+                uint lweight = rweight = 0;
 
                 for (i = 0; i < num_node; i++)
                 {
@@ -342,13 +317,11 @@ namespace Fire_Emblem_Awakening_Save_Tool
                 lnode.dad = rnode.dad = node;
             }
         }
-
         private void HUF_InitCodeTree()
         {
-            uint max_nodes;
             uint i;
 
-            max_nodes = (((num_leafs - 1) | 1) + 1) << 1;
+            uint max_nodes = (((num_leafs - 1) | 1) + 1) << 1;
 
             codetree = new byte[max_nodes];
             codemask = new byte[max_nodes];
@@ -359,12 +332,9 @@ namespace Fire_Emblem_Awakening_Save_Tool
                 codemask[i] = 0;
             }
         }
-
         private void HUF_CreateCodeTree()
         {
-            uint i;
-
-            i = 0;
+            uint i = 0;
 
             codetree[i] = (byte)((num_leafs - 1) | 1);
             codemask[i] = 0;
@@ -375,21 +345,20 @@ namespace Fire_Emblem_Awakening_Save_Tool
             i = (uint)((codetree[0] + 1) << 1);
             while (--i != 0) if (codemask[i] != 0xFF) codetree[i] |= codemask[i];
         }
-
         private uint HUF_CreateCodeBranch(HuffmanNode root, uint p, uint q)
         {
             HuffmanNode[] stack = new HuffmanNode[2 * root.leafs];
-            uint r, s, mask;
-            uint l_leafs, r_leafs;
+            uint mask;
 
             if (root.leafs <= HUF_NEXT + 1)
             {
-                HuffmanNode node;
-                s = r = 0;
+                uint r;
+                uint s = r = 0;
                 stack[r++] = root;
 
                 while (s < r)
                 {
+                    HuffmanNode node;
                     if ((node = stack[s++]).leafs == 1)
                     {
                         if (s == 1) { codetree[p] = (byte)node.symbol; codemask[p] = 0xFF; }
@@ -419,31 +388,29 @@ namespace Fire_Emblem_Awakening_Save_Tool
 
                 if (root.lson.leafs <= root.rson.leafs)
                 {
-                    l_leafs = HUF_CreateCodeBranch(root.lson, q, q + 2);
-                    r_leafs = HUF_CreateCodeBranch(root.rson, q + 1, q + (l_leafs << 1));
+                    uint l_leafs = HUF_CreateCodeBranch(root.lson, q, q + 2);
+                    // r_leafs = HUF_CreateCodeBranch(root.rson, q + 1, q + (l_leafs << 1));
                     codetree[q + 1] = (byte)(l_leafs - 1);
                 }
                 else
                 {
-                    r_leafs = HUF_CreateCodeBranch(root.rson, q + 1, q + 2);
-                    l_leafs = HUF_CreateCodeBranch(root.lson, q, q + (r_leafs << 1));
+                    uint r_leafs = HUF_CreateCodeBranch(root.rson, q + 1, q + 2);
+                    // l_leafs = HUF_CreateCodeBranch(root.lson, q, q + (r_leafs << 1));
                     codetree[q] = (byte)(r_leafs - 1);
                 }
             }
 
             return (root.leafs);
         }
-
         private void HUF_UpdateCodeTree()
         {
-            uint max, inc, n0, n1, l0, l1, tmp0, tmp1;
-            uint i, j, k;
-
-            max = (uint)((codetree[0] + 1) << 1);
+            uint i;
+            uint max = (uint)((codetree[0] + 1) << 1);
             for (i = 1; i < max; i++)
             {
                 if ((codemask[i] != 0xFF) && (codetree[i] > HUF_NEXT))
                 {
+                    uint inc;
                     if ((i & 1) != 0 && (codetree[i - 1] == HUF_NEXT))
                     {
                         i--;
@@ -459,14 +426,15 @@ namespace Fire_Emblem_Awakening_Save_Tool
                         inc = codetree[i] - HUF_NEXT;
                     }
 
-                    n1 = (i >> 1) + 1 + codetree[i];
-                    n0 = n1 - inc;
+                    uint n1 = (i >> 1) + 1 + codetree[i];
+                    uint n0 = n1 - inc;
 
-                    l1 = n1 << 1;
-                    l0 = n0 << 1;
+                    uint l1 = n1 << 1;
+                    uint l0 = n0 << 1;
 
-                    tmp0 = BitConverter.ToUInt16(codetree, (int)l1);
-                    tmp1 = BitConverter.ToUInt16(codemask, (int)l1);
+                    uint tmp0 = BitConverter.ToUInt16(codetree, (int)l1);
+                    uint tmp1 = BitConverter.ToUInt16(codemask, (int)l1);
+                    uint j;
                     for (j = l1; j > l0; j -= 2)
                     {
                         Array.Copy(BitConverter.GetBytes(BitConverter.ToUInt16(codetree, (int)j - 2)), 0, codetree, (int)j, 2);
@@ -477,6 +445,7 @@ namespace Fire_Emblem_Awakening_Save_Tool
 
                     codetree[i] -= (byte)inc;
 
+                    uint k;
                     for (j = i + 1; j < l0; j++)
                     {
                         if (codemask[j] != 0xFF)
@@ -508,27 +477,23 @@ namespace Fire_Emblem_Awakening_Save_Tool
             codes = new HuffmanCode[max_symbols];
             for (i = 0; i < max_symbols; i++) codes[i] = null;
         }
-
         private void HUF_CreateCodeWorks()
         {
-            uint symbol, nbits, maxbytes, nbit;
             byte[] scode = new byte[100];
-            byte mask;
-            uint i, j;
+            uint i;
 
             for (i = 0; i < num_leafs; i++)
             {
-                HuffmanNode node;
-                node = tree[i];
-                symbol = node.symbol;
+                HuffmanNode node = tree[i];
+                uint symbol = node.symbol;
 
-                nbits = 0;
+                uint nbits = 0;
                 while (node.dad != null)
                 {
                     scode[nbits++] = node.dad.lson == node ? (byte)HUF_LNODE : (byte)HUF_RNODE;
                     node = node.dad;
                 }
-                maxbytes = (nbits + 7) >> 3;
+                uint maxbytes = (nbits + 7) >> 3;
 
                 HuffmanCode code = new HuffmanCode();
 
@@ -536,10 +501,12 @@ namespace Fire_Emblem_Awakening_Save_Tool
                 code.nbits = nbits;
                 code.codework = new byte[maxbytes];
 
+                uint j;
                 for (j = 0; j < maxbytes; j++) code.codework[j] = 0;
 
-                mask = (byte)HUF_MASK;
+                byte mask = (byte)HUF_MASK;
                 j = 0;
+                uint nbit;
                 for (nbit = nbits; nbit != 0; nbit--)
                 {
                     if (scode[nbit - 1] != 0) code.codework[j] |= mask;
